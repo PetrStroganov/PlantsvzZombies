56931419b72555cf6f28bca73d3c1860bff1f8f9
Fishbet1 <vila19283@gmail.com>
2024-12-22 23:30:30 +0300

    добавлен прицел

--- pvz.py
+++ pvz.py
@@ -1,5 +1,8 @@
+import pygame
+
 from models.interface.hud import HUD
 from models.interface.map import *
+from models.cursor import Cursor
 
 
 if __name__ == '__main__':
@@ -10,15 +13,26 @@ if __name__ == '__main__':
     hud = HUD()
     field = Field()
     isShowHitbox = True
+
     clock = pygame.time.Clock()
+
+    cursor = Cursor()
+    pygame.mouse.set_visible(False)
+
     running = True
+
     while running:
         tick = clock.tick()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+            if event.type == pygame.MOUSEMOTION:
+                cursor.move(*event.pos)
+
         screen.fill((0, 0, 0))
         map.draw(screen)
         hud.draw(screen, is_show_hitbox=isShowHitbox)
         field.draw(screen)
+        cursor.draw(screen)
         pygame.display.flip()
