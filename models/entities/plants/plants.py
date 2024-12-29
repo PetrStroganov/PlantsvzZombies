d4950f970f14f24aa6656bb6bdabd72b1e66bba7
PetrStroganov <kojlovpetr@gmail.com>
2024-12-29 16:25:13 +0300

    заготовка для растений

--- models/entities/plants/plants.py
+++ models/entities/plants/plants.py
@@ -2,7 +2,17 @@ import pygame
 
 
 class Plant:
-    pass
+    def __init__(self, health, cost):
+        self.health = health
+        self.cost = cost
+        self.line = 0
+        self.column = 0
+        self._ = pygame.sprite.GroupSingle()
+        self._.add(self.sprite)
+
+    def draw(self, screen: pygame.Surface):
+        if self.health > 0:
+            self._.draw(screen)
 
 
 class SunFlower(Plant):
