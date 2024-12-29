656948614710ac85bf92f24da3eb340a6d41102b
PetrStroganov <kojlovpetr@gmail.com>
2024-12-29 15:35:42 +0300

    добавлено количество солнца, создан main.py

--- pvz.py
+++ pvz.py
@@ -1,38 +1,50 @@
 import pygame
+import time
 
 from models.interface.hud import HUD
 from models.interface.map import *
 from models.cursor import Cursor
+from models.map.other import *
+
+EXIT, MAIN_SCREEN = 0, 1
 
 
-if __name__ == '__main__':
-    pygame.init()
-    size = w, h = 1000, 600
-    screen = pygame.display.set_mode(size)
+def game(screen):
     map = Map()
     hud = HUD()
     field = Field()
+    falling_sun_test = FallingSun()
+    falling_time = time.time()
     isShowHitbox = True
-
     clock = pygame.time.Clock()
-
     cursor = Cursor()
     pygame.mouse.set_visible(False)
-
     running = True
+    suns_count = 250
+    font = pygame.font.SysFont(None, 40)
 
     while running:
         tick = clock.tick()
+        # Обрабатываем события
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-
+                return EXIT
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_F1:
+                    isShowHitbox = not isShowHitbox
             if event.type == pygame.MOUSEMOTION:
                 cursor.move(*event.pos)
 
-        screen.fill((0, 0, 0))
+        screen.fill((100, 100, 100))
         map.draw(screen)
         hud.draw(screen, is_show_hitbox=isShowHitbox)
         field.draw(screen)
+        if time.time() - falling_time > 10:
+            falling_sun = FallingSun()
+            falling_sun.draw(screen)
+            falling_time = time.time()
         cursor.draw(screen)
+        falling_sun_test.draw(screen)
+        suns_text = font.render(str(suns_count), True, (0, 0, 0))
+        screen.blit(suns_text, (45, 82))
         pygame.display.flip()
